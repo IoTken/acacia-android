9619c60a16b8b070c4dfde840b1f8f5453c565bd
saitoiotken <saitoiotken@users.noreply.github.com>
2018-06-19 22:27:12 +0900

    add logout

--- app/src/main/java/com/acacia/iotken/acacia/MainActivity.java
+++ app/src/main/java/com/acacia/iotken/acacia/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.content.Intent;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -21,61 +26,152 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "MainActivity";
+    private boolean mSingInFailsFlag = false;
+    private boolean mIsSingInProcessesFlag = false;
+
     private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .build();
 
-        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        Log.d("テスト","account:" + account);
 
-        signIn();
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d(TAG,"currentUser:" + currentUser);
+        updateUI(currentUser);
+
+        if (currentUser == null && !mIsSingInProcessesFlag) {
+            if (mSingInFailsFlag) {
+                singInFailsDialog();
+            } else {
+                signIn();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            signOut();
+
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            Log.d(TAG,"action_logout:" + account);
+            signIn();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.w(TAG, "account:" + account);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                mSingInFailsFlag = true;
+                mIsSingInProcessesFlag = false;
+                updateUI(null);
+            }
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Log.d("login","account:" + account);
+    // Firebase
+    private void signIn() {
+        mIsSingInProcessesFlag = true;
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-            // Signed in successfully, show authenticated UI.
-//            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("login", "signInResult:failed code=" + e.getStatusCode());
-//            updateUI(null);
-        }
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d(TAG, "signInWithCredential:success:" + user.getDisplayName());
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            mSingInFailsFlag = true;
+                            updateUI(null);
+                        }
+                        mIsSingInProcessesFlag = false;
+                    }
+                });
+    }
+
+    //Dialog
+    private void singInFailsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("ログインに失敗しました")
+                .setCancelable(false)
+                .setPositiveButton("再ログイン", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.d(TAG,"login");
+                        mSingInFailsFlag = false;
+                        dialog.dismiss();
+                        signIn();
+                    }
+                });
+        builder.show();
+    }
+
+    private void updateUI(FirebaseUser user) {
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(user == null ? "未ログイン" : user.getDisplayName());
     }
 }
